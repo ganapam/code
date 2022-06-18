6fbf45819da5dab79ce580af5357a582c0a270b1
MAHENDRA REDDY <51808572+ganapam@users.noreply.github.com>
2022-06-18 11:10:58 +0530

    Create Random_number_samll_program.py

---
+++ python/Random_number_samll_program.py
@@ -0,0 +1,12 @@
+# pickup random numbers
+import random
+n=random.randint(1,10)    # random int it picking random integer
+print(n)
+print(random.random( ) )  # float it print random wise float value
+a=random.randint(1,3)
+if a==1:
+    print("yes")
+if a==2:
+    print("no")
+if a==3:
+    print("may be")
